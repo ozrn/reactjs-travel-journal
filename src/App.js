0673e3c657981301007f7a33ae5a3c89b2b0d047
ozrn <ozerenkevser92@gmail.com>
2022-11-13 17:00:41 +0300

    Add project files

---
+++ src/App.js
@@ -0,0 +1,6 @@
+export default function App() {
+  return (
+
+    <h1>Welcome to my travel journal</h1>
+  )
+}
---
+++ src/data.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        title: "Anıtkabir",
+        location: "Türkiye",
+        startDate: "01 Oct, 2022",
+        endDate: "14 Oct, 2022",
+        description: "Anıtkabir is the mausoleum of Mustafa Kemal Atatürk, the leader of the Turkish War of Independence and the founder and the first President of the Republic of Turkey.",
+        imageUrl: "https://unsplash.com/photos/ANMh1PSKO_k"
+    },
+    {
+        title: "Traverse City",
+        location: "USA",
+        startDate: "6 Apr, 2023",
+        endDate: "28 Apr, 2023",
+        description: "Traverse City is a city in the U.S. state of Michigan. The Traverse City area features varied natural attractions, including freshwater beaches, vineyards, a National Lakeshore, downhill skiing areas, and numerous forests.",
+        imageUrl: "https://unsplash.com/photos/EpFjmHIIJgo"
+    },
+    {
+        title: "Warsaw",
+        location: "Poland",
+        startDate: "12 Sept, 2023",
+        endDate: "30 Sept, 2023",
+        description: "Warsaw is the capital and largest city of Poland. The metropolis stands on the River Vistula in east-central Poland.",
+        imageUrl: "https://unsplash.com/photos/Oirxgq5IyCI"
+    },
+    {
+        title: "Reykjavík",
+        location: "Iceland",
+        startDate: "14 Feb, 2023",
+        endDate: "24 Feb, 2023",
+        description: "Reykjavík is the capital and largest city of Iceland and it is the centre of Iceland's cultural, economic, and governmental activity, and is a popular tourist destination.",
+        imageUrl: "https://unsplash.com/photos/WvPQYDd-3Ow"
+    }
+]
